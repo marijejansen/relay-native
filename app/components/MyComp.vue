578ae5709b42bd45f4427d79e7b43fbc11662d71
Marije Jansen <marije.jansen@mailbox.org>
2020-04-14 19:52:03 +0200

    first commit

---
+++ app/components/MyComp.vue
@@ -0,0 +1,11 @@
+<template>
+  <Label class="message" :text="msg" col="0" row="0"/>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+@Component
+export default class MyComp extends Vue {
+  private msg: string = 'YESYESYESYES';
+}
+</script>
