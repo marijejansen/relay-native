0c497e47965a97e8978db1ea9976759cf0c5f918
Marije Jansen <marije.jansen@mailbox.org>
2020-04-15 16:13:17 +0200

    layout search

--- app/components/Search.vue
+++ app/components/Search.vue
@@ -1,14 +1,23 @@
 <template>
-  <GridLayout columns="*, *" rows="*, *">
-    <TextField row="0" col="0" v-model="firstName" hint="First name" />
-    <TextField row="0" col="1" v-model="lastName" hint="Last name" />
-    <Button text="Search" row="1" @tap="search"></Button>
-    <StackLayout class="search-results" row="2">
-      <StackLayout orientation="horizontal" v-for="test in results()" v-bind:key="test.id">
-        <Label :text="test.firstName"></Label>
-        <Label :text="test.lastName"></Label>
-      </StackLayout>
-    </StackLayout>
+  <GridLayout rows="auto, *, auto">
+
+  <GridLayout row="0" orientation="horizontal" columns="*,*">
+    <TextField column="0" v-model="firstName" hint="First name" />
+    <TextField column="1" v-model="lastName" hint="Last name" />
+  </GridLayout>
+
+    <ListView row="1" for="result in results()">
+      <v-template>
+        <FlexboxLayout flexDirection="row" style="background-color: greenyellow">
+          <Label :text="result.firstName"></Label>
+          <Label :text="result.lastName"></Label>
+          <Label :text="result.clubName"></Label>
+          <Label :text="result.birthYear"></Label>
+        </FlexboxLayout>
+      </v-template>
+    </ListView>
+
+  <Button row="2" text="Search" @tap="search"></Button>
   </GridLayout>
 </template>
 
