f44f28d1519a039d679bbd668080314d01f784d7
Marije Jansen <marije.jansen@mailbox.org>
2020-04-18 10:07:38 +0200

    added scss files

--- app/components/calculate/Calculate.ts
+++ app/components/calculate/Calculate.ts
@@ -1,5 +1,5 @@
 import { Component, Vue } from "vue-property-decorator";
-import './Search.scss'
+import './Calculate.scss'
 import { Swimmer } from '@/models/Swimmer';
 import searchRepository from '@/repositories/search-repository'
 import store from '@/store/index';
