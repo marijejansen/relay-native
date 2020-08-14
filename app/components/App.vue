7e2c972735f4f011f4a202ce69061a2c03d12ae5
Marije Jansen <marije.jansen@mailbox.org>
2020-08-14 16:38:28 +0200

    cant get sidedrawer to work

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,7 +1,14 @@
 <template>
   <Page class="page">
-    <ActionBar :title="$t('actionbar.message')" android:flat="true" />
+    <ActionBar :title="$t('actionbar.message')" android:flat="true">
+      <GridLayout columns="5*, *">
+        <Label class="title" :text="$t('actionbar.message')" col="0" />
+        <Label text="(())" @tap="toggleOptions" col="1" />
+      </GridLayout>
+    </ActionBar>/>
+    <Options v-if="optionsOpen"></Options>
     <TabView
+      v-else
       android:tabBackgroundColor="#29648A"
       android:tabTextColor="#c4ffdf"
       android:selectedTabTextColor="#ffffff"
@@ -23,21 +30,34 @@
 import Search from "./search/Search";
 import Times from "./times/Times";
 import Calculate from "./calculate/Calculate";
+import Options from "./options/Options";
 
 export default {
   components: {
     Search,
     Times,
     Calculate,
+    Options
   },
   data() {
-    return {};
+    return {
+      optionsOpen: false,
+    };
+  },
+  methods: {
+      toggleOptions: function () {
+        this.optionsOpen = !this.optionsOpen;
+    }
   },
 };
 </script>
 
 <style scoped>
 ActionBar {
+  background-color: #29648a;
+  color: #ffffff;
+}
+GridLayout > * {
   background-color: #29648a;
   color: #ffffff;
 }
---
+++ app/components/options/Options.vue
@@ -0,0 +1,9 @@
+<template>
+<Label text="OPTIONS"/>
+</template>    
+
+
+<script lang="ts">
+//@ts-ignore
+  export { default } from "./Options.ts";
+</script>
