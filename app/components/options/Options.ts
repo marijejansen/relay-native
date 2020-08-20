b80f8d7de91038103ed3387e170f27c26bd84eb9
Marije Jansen <marije.jansen@mailbox.org>
2020-08-20 15:55:21 +0200

    options page

--- app/components/options/Options.ts
+++ app/components/options/Options.ts
@@ -1,8 +1,54 @@
 import { Vue, Component } from "vue-property-decorator";
+import './Options.scss'
+import store from '@/store/index';
+import translate from '@/locales/i18n'
+import Selector from '@/components/selectors/Selector';
 
 
-@Component({})
+@Component({ components: { Selector}})
 export default class Options extends Vue {
+    
+    private isMasters: number = Number(store.getters['isMasters']);
 
+    private forYear: number = Number(store.getters['getForYear']);
+
+    get yearIndex(): number{
+        var years = this.getForYearItems;
+        var year = this.forYear.toString();
+        var index = years.findIndex(y => y === year);
+        return index;
+    }
+
+    get isMastersItems(): string[] {
+        var items: string[] = [translate.t('options.ageGroups.allin').toString(), translate.t('options.ageGroups.masters').toString()];
+        console.log("      isMasters:      " + Number(this.isMasters));
+
+        return items;
+    }
+
+    get getForYearItems(): string[] {
+        var thisYear = (new Date()).getFullYear();
+        var years: string[] = [];
+        for(var i = 0; i < 4; i++){
+            years.push((thisYear + i).toString())
+        }
+
+        return years;
+    }
+
+    setForYear(yearIndex: number) {
+        var year = this.getForYearItems[yearIndex];
+        store.commit('setForYear', Number(year));
+        this.emptyRelayTeams();
+    }
+
+    setIsMasters(isMasters: number) {
+        store.commit('setIsMasters', isMasters);
+        this.emptyRelayTeams();
+    }
+
+    emptyRelayTeams(){
+        store.commit("calculate/emptyRelayTeams");
+    }
 
 }
--- app/components/options/Options.vue
+++ app/components/options/Options.vue
@@ -1,5 +1,14 @@
-<template>
-<Label text="OPTIONS"/>
+<template class="options">
+
+<GridLayout rows="auto, auto, auto, auto, auto">
+    <Label row="0" text="OPTIONS"/>
+    <Label row="1" text="Age groups:"/>
+    <Selector row="2" :active-number="isMasters" :items="isMastersItems" @changeItem="setIsMasters"></Selector>
+    <Label row="3" text="Calculate for year:"/>
+    <Selector row="4" :active-number="yearIndex" :items="getForYearItems" @changeItem="setForYear"></Selector>
+
+</GridLayout>
+
 </template>    
 
 
