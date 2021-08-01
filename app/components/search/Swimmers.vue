4fc7dbbad2a3d17daad38521ebac266061daab11
Marije Jansen <marije.jansen@winvision.nl>
2021-08-01 13:14:12 +0200

    some styling

--- app/components/search/Swimmers.vue
+++ app/components/search/Swimmers.vue
@@ -1,9 +1,9 @@
 <template>
   <GridLayout class="container swimmers-layout" rows="auto, auto, auto">
     <Selector row="0" :active-number="selectedPage" :items="pageSelectionItems" @changeItem="setPage" ></Selector>
-    <Search height="30%" v-if="showSearch" row="1"></Search>
-    <ManualAdd height="30%" v-if="showManual" row="1"></ManualAdd>
-    <Selection height="70%" row="2" v-if="hasSelection()"></Selection>
+    <Search  v-if="showSearch" row="1"></Search>
+    <ManualAdd v-if="showManual" row="1"></ManualAdd>
+    <Selection row="2" v-if="hasSelection()"></Selection>
   </GridLayout>
 </template>
 
